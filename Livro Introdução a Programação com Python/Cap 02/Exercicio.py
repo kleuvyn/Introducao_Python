8d6f7c00e777b7498fbd828dc87d4b323ad85378
kleuvyn <kleuvyn@hotmail.com>
2023-09-15 20:21:00 -0300

    up

---
+++ Livro Introdução a Programação com Python/Cap 02/Exercicio.py
@@ -0,0 +1,11 @@
+# Exercício 02-01
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o
+# interpretador Python. 10 + 20 × 30 42 ÷ 30 (94 + 2) × 6 - 1
+
+test = 10 + 20 * 30
+print(test)
+
+# Exercício 02-02
+# Digite a seguinte expressão no interpretador: 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2 Tente resolver o
+# mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é
+# importante.
