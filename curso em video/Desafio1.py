64e0d6001048135515fbbbdb33f6fbe01385b2ff
kleuvyn <kleuvyn@hotmail.com>
2023-12-05 21:33:37 -0300

    revisando

---
+++ curso em video/Desafio1.py
@@ -0,0 +1,21 @@
+# Desafio 1
+# Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = str(input('Qual é o seu nome? '))
+print(f'Olá {nome}! É um prazer tê-lo(a) aqui. Este é um espaço para compartilhar conhecimentos, aprender juntos e aproveitar a jornada. Fique à vontade para participar das conversas, fazer perguntas e contribuir com suas experiências. Estamos todos aqui para nos apoiar. Seja bem-vindo(a) à nossa comunidade! 🚀✨')
+
+# Desafio 2
+# Crie um script que leia o dia, mês e o ano da data de nascimento e mostre uma mensagem formatada
+
+dia = int(input('Qual dia você nasceu: '))
+mes = int(input('Qual mês você nasceu: '))
+ano = int(input('Qual ano você nasceu: '))
+print(f'Olá {nome}, você nasceu em {dia}/{mes}/{ano}.')
+
+# Desafio 3
+# Crie um script que leia dois números e mostre a soma entre eles
+print('Vamos somar!!!')
+primeiro_numero = int(input('Primeiro número: '))
+segundo_numero = int(input('Segundo número: '))
+soma = primeiro_numero + segundo_numero
+print(f'A soma de {primeiro_numero} + {segundo_numero} é igual a {soma}.')
---
+++ curso em video/Primeiros comandos.py
@@ -0,0 +1,13 @@
+# print imprime e mostra na tela 
+print('Olá Mundo!')
+
+# input recebe uma informacao 
+nome = input('Qual é o seu nome: ')
+idade = int(input('Qual é a sua idade: '))
+print(f'Olá {nome}!')
+
+# soma
+print(7+4) 
+
+# concatenar unir o conteúdo de duas strings
+print('7' + '4')
