f1fc776f84465b0ddc708ba2a38171b293d47204
kleuvyn <kleuvyn@hotmail.com>
2023-12-05 21:35:16 -0300

    revisando

--- curso em video/Desafio1.py
+++ curso em video/Desafio1.py
@@ -1,8 +1,10 @@
 # Desafio 1
-# Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+#Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
 
 nome = str(input('Qual é o seu nome? '))
-print(f'Olá {nome}! É um prazer tê-lo(a) aqui. Este é um espaço para compartilhar conhecimentos, aprender juntos e aproveitar a jornada. Fique à vontade para participar das conversas, fazer perguntas e contribuir com suas experiências. Estamos todos aqui para nos apoiar. Seja bem-vindo(a) à nossa comunidade! 🚀✨')
+boas_vindas = f'Olá {nome}! Seja muito bem-vinda! Este é um espaço para compartilhar conhecimentos, aprender juntos e aproveitar a jornada. Sinta-se à vontade para participar das conversas, fazer perguntas e contribuir com suas experiências. Estamos aqui para nos apoiar mutuamente. Seja bem-vinda à nossa comunidade! 🚀✨'
+
+print(boas_vindas)
 
 # Desafio 2
 # Crie um script que leia o dia, mês e o ano da data de nascimento e mostre uma mensagem formatada
@@ -10,12 +12,16 @@ print(f'Olá {nome}! É um prazer tê-lo(a) aqui. Este é um espaço para compar
 dia = int(input('Qual dia você nasceu: '))
 mes = int(input('Qual mês você nasceu: '))
 ano = int(input('Qual ano você nasceu: '))
-print(f'Olá {nome}, você nasceu em {dia}/{mes}/{ano}.')
+nascimento = f'Olá {nome}, você nasceu em {dia}/{mes}/{ano}.'
+
+print(nascimento)#
 
 # Desafio 3
 # Crie um script que leia dois números e mostre a soma entre eles
 print('Vamos somar!!!')
 primeiro_numero = int(input('Primeiro número: '))
 segundo_numero = int(input('Segundo número: '))
-soma = primeiro_numero + segundo_numero
-print(f'A soma de {primeiro_numero} + {segundo_numero} é igual a {soma}.')
+soma_numeros = primeiro_numero + segundo_numero
+mensagem_soma = f'A soma de {primeiro_numero} + {segundo_numero} é igual a {soma_numeros}.'
+
+print(mensagem_soma)
