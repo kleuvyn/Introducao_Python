b45a52dba8c98443722a8b8e9cc7d6f702bb4391
kleuvyn <kleuvyn@hotmail.com>
2024-02-25 22:28:21 -0300

    atualizando

---
+++ Desafios /#1/script cont.js
@@ -0,0 +1,59 @@
+const switchModal = () => {
+  const modal = document.querySelector('.modal')
+  const actualStyle = modal.style.display
+  if(actualStyle == 'block') {
+    modal.style.display = 'none'
+  } else {
+    modal.style.display = 'block'
+  }
+}
+
+const btnConfirmar = document.getElementById('confirmarBtn');
+btnConfirmar.addEventListener('click', function() {
+  const valores = obterValoresFormulario();
+  const corpoEmail = construirCorpoEmail(valores);
+  const assunto = 'Nova mensagem de contato';
+
+  const mailtoLink = `mailto:b.kleuvyn@gmail.com?subject=${encodeURIComponent(
+    assunto
+  )}&body=${encodeURIComponent(corpoEmail)}`;
+
+  window.location.href = mailtoLink;
+
+  switchModal(); 
+});
+
+window.onclick = function(event) {
+  const modal = document.querySelector('.modal')
+  if (event.target == modal) {
+    switchModal();
+  }
+}
+
+function obterValoresFormulario() {
+  const nome = document.getElementById('texto').value;
+  const telefone = document.getElementById('telefone').value;
+  const hora = document.getElementById('hora').value;
+  const primeiroContatoSim = document.getElementById('sim').checked;
+  const primeiroContatoNao = document.getElementById('nao').checked;
+  const mensagem = document.getElementById('mensagem').value;
+
+  return {
+    nome,
+    telefone,
+    hora,
+    primeiroContatoSim,
+    primeiroContatoNao,
+    mensagem,
+  };
+}
+
+function construirCorpoEmail(valores) {
+  return `
+    Nome: ${valores.nome}
+    Telefone: ${valores.telefone}
+    Horário para entrar em contato: ${valores.hora}
+    Primeiro Contato: ${valores.primeiroContatoSim ? 'Sim' : 'Não'}
+    Mensagem: ${valores.mensagem}
+  `;
+}
